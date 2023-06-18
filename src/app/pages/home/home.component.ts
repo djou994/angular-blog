097c6655273d36451c2d7599def176f5a6ce2471
jonathan.ehmke <jonathan.ehmke@partners.mv.com.br>
2023-06-18 19:34:27 -0300

    content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {dataFake} from "../../data/dataFake";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  mainTitle:string = "";
+  mainDescription: string = "";
+  mainPhoto:string = "";
+  content: ({ description: string; photo: string; id: string; title: string } )[] = [];
 
+  ngOnInit(): void {
+    this.mainTitle = dataFake[0].title;
+    this.mainDescription = `${dataFake[0].description.substring(0,100)}...`;
+    this.mainPhoto = dataFake[0].photo;
+
+    this.content = dataFake.slice(1);
+  }
 }
